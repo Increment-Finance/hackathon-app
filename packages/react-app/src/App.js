966c5ef4c694658b30636e4c2ec15909d0a94b05
Owen Murovec <owenmurovec@gmail.com>
2021-06-21 13:45:34 -0700

    Prep merge

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -7,6 +7,48 @@ import TradingViewWidget, { Themes } from "react-tradingview-widget";
 import { Dropdown, message } from "antd";
 import { DownOutlined } from "@ant-design/icons";
 import { Descriptions } from "antd";
+import { Contract } from "@ethersproject/contracts";
+import { getDefaultProvider } from "@ethersproject/providers";
+import { useQuery } from "@apollo/react-hooks";
+
+import useWeb3Modal from "./hooks/useWeb3Modal";
+
+import { addresses, abis } from "@project/contracts";
+import GET_TRANSFERS from "./graphql/subgraph";
+
+async function readOnChainData() {
+  // Should replace with the end-user wallet, e.g. Metamask
+  const defaultProvider = getDefaultProvider();
+  // Create an instance of an ethers.js Contract
+  // Read more about ethers.js on https://docs.ethers.io/v5/api/contract/contract/
+  const ceaErc20 = new Contract(
+    addresses.ceaErc20,
+    abis.erc20,
+    defaultProvider
+  );
+  // A pre-defined address that owns some CEAERC20 tokens
+  const tokenBalance = await ceaErc20.balanceOf(
+    "0x3f8CB69d9c0ED01923F11c829BaE4D9a4CB6c82C"
+  );
+  console.log({ tokenBalance: tokenBalance.toString() });
+}
+
+function WalletButton({ provider, loadWeb3Modal, logoutOfWeb3Modal }) {
+  return (
+    <button
+      onClick={() => {
+        console.log({ provider });
+        if (!provider) {
+          loadWeb3Modal();
+        } else {
+          logoutOfWeb3Modal();
+        }
+      }}
+    >
+      {!provider ? "Connect Wallet" : "Disconnect Wallet"}
+    </button>
+  );
+}
 
 const { Header, Content, Footer } = Layout;
 const onClick = ({ key }) => {
@@ -22,6 +64,14 @@ const menu = (
 );
 
 function App() {
+  const { loading, error, data } = useQuery(GET_TRANSFERS);
+  const [provider, loadWeb3Modal, logoutOfWeb3Modal] = useWeb3Modal();
+
+  React.useEffect(() => {
+    if (!loading && !error && data && data.transfers) {
+      console.log({ transfers: data.transfers });
+    }
+  }, [loading, error, data]);
   return (
     <Layout>
       <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
@@ -36,6 +86,14 @@ function App() {
         className="site-layout"
         style={{ padding: "0 50px", height: 1000, marginTop: 64 }}
       >
+        <WalletButton
+          provider={provider}
+          loadWeb3Modal={loadWeb3Modal}
+          logoutOfWeb3Modal={logoutOfWeb3Modal}
+        />
+        <button hidden onClick={() => readOnChainData()}>
+          Read On-Chain Balance
+        </button>
         <div
           className="site-layout-background"
           style={{ padding: 12, minHeight: 380 }}
@@ -44,7 +102,7 @@ function App() {
             <a
               href="/"
               className="ant-dropdown-link"
-              onClick={(e) => e.preventDefault()}
+              onClick={e => e.preventDefault()}
             >
               EUR/USD <DownOutlined />
             </a>
--- packages/react-app/src/index.js
+++ packages/react-app/src/index.js
@@ -1,14 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
 import "./index.css";
 import App from "./App";
 
 import "antd/dist/antd.css";
 import "./index.css";
 
+// You should replace this url with your own and put it into a .env file
+// See all subgraphs: https://thegraph.com/explorer/
+const client = new ApolloClient({
+  uri: "https://api.thegraph.com/subgraphs/name/paulrberg/create-eth-app"
+});
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById("root")
 );
