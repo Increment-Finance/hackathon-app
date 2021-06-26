f6e859250738e06ee359d54721aa67cba4bf6b21
Owen Murovec <owenmurovec@gmail.com>
2021-06-26 11:40:31 -0700

    Add ENS resolution

--- packages/react-app/src/components/Header.js
+++ packages/react-app/src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { WalletButton } from ".";
 import { useHistory, useLocation } from "react-router-dom";
 import "./Header.scss";
@@ -11,6 +11,7 @@ export default function Header({
 }) {
   const { pathname } = useLocation();
   const history = useHistory();
+  const [ens, setEns] = useState();
   const displayAddress = address
     ? `${address.slice(0, 6)}...${address.slice(37, 41)}`
     : "";
@@ -18,6 +19,20 @@ export default function Header({
   const goTo = href => {
     history.push(href);
   };
+
+  useEffect(() => {
+    let subscribed = true;
+    if (provider && address) {
+      provider.lookupAddress(address).then(result => {
+        if (subscribed) {
+          setEns(result);
+        }
+      });
+    }
+    return () => {
+      subscribed = false;
+    };
+  }, [provider, address]);
 
   return (
     <div id="header">
@@ -37,7 +52,7 @@ export default function Header({
       >
         Dashboard
       </h2>
-      <p className="address">{displayAddress}</p>
+      <p className="address">{ens || displayAddress}</p>
       <WalletButton
         provider={provider}
         loadWeb3Modal={loadWeb3Modal}
