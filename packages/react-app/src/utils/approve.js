a27542339c74f73d7deb46da3c31985e1c4a98e6
Owen Murovec <owenmurovec@gmail.com>
2021-07-01 15:52:58 -0700

    Clean up deposit

--- packages/react-app/src/utils/approve.js
+++ packages/react-app/src/utils/approve.js
@@ -2,9 +2,18 @@ import erc20 from "../contracts/erc20.abi";
 import perpetualAddress from "../contracts/Perpetual.address";
 import { Contract } from "@ethersproject/contracts";
 
-export default async function approve(provider, tokenAddress, amount) {
-  await new Contract(tokenAddress, erc20, provider.getSigner()).approve(
-    perpetualAddress,
-    amount
-  );
+export default function approve(provider, tokenAddress, amount, userAddress) {
+  return new Promise(async (resolve, reject) => {
+    const contract = new Contract(tokenAddress, erc20, provider.getSigner());
+    await contract.approve(perpetualAddress, amount);
+    contract.on("Approval", (address, spender, value) => {
+      if (
+        userAddress === address &&
+        value.toNumber() === amount.toNumber() &&
+        spender === perpetualAddress
+      ) {
+        resolve({ address, spender, value });
+      }
+    });
+  });
 }
