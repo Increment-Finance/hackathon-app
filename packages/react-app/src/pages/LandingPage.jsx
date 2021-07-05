37f7d263b7ed83fbb9a1b24b3363faec628c6d9c
Chinmay Gopal <chinmayg015@gmail.com>
2021-07-05 16:34:11 +0530

    Update LandingPage.jsx

--- packages/react-app/src/pages/LandingPage.jsx
+++ packages/react-app/src/pages/LandingPage.jsx
@@ -3,36 +3,44 @@ import { useHistory, useLocation } from "react-router-dom";
 import "./LandingPage.scss";
 import logo from "../assets/top_logo.png";
 import button from "../assets/button1.png";
+import button2 from "../assets/button2.png";
 
 export default function Market({}) {
   const history = useHistory();
-
   const goTo = (href) => {
     history.push(href);
   };
 
   return (
+
     <div className="landing">
       <div className="topbar">
+
         <img className="topbar-img" src={logo} alt="Logo" />{" "}
-        <img
-          className="img-button topbar-img"
+        <button class="Docs">Docs</button>
+
+        <img className="topbar-img"
           src={button}
           alt="button"
           onClick={() => {
             goTo("/market");
           }}
         />
+
+
       </div>
+
       <div className="content">
-        <h1 className="title"> On-Chain Forex Derivatives Protocol </h1>
+
+        <div className="title"> On-Chain Forex {"\n"} Derivatives Protocol </div>
+
         <p className="description">
           Empowering retail and institutional crypto holders including suppliers
-          & borrowers on Aave to hedge against forex price movements.
+          & borrowers {"\n"} on Aave to hedge against forex price movements.
         </p>
         <img
           className="img-button"
-          src={button}
+          src={button2}
           width={300}
           alt="button"
           onClick={() => {
